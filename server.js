bbb22fddcf007a5940ff6bc604e955d33040c620
sadokist <singhga@oregonstate.edu>
2021-06-16 18:42:42 -0700

    cool

--- server.js
+++ server.js
@@ -1,1 +1,36 @@
 
+var express = require("express")
+var path = require('path');
+var exphbs = require("express-handlebars")
+
+
+
+var app = express()
+
+var port = process.env.PORT || 3000;
+
+app.engine("handlebars", exphbs({defaultLayout: "main"}))
+app.set("view engine", "handlebars")
+
+
+app.get('/', function (req, res) {
+    res.status(200).render("home", {
+    })
+    
+});
+
+app.use(express.static('public'));
+
+app.get('/members', function (req, res) {
+    res.status(200).render("members", {
+    })
+    
+});
+
+app.get('*', function (req, res) {
+    res.status(404).render("404", {page: req.url})
+  });
+
+app.listen(port, function () {
+    console.log("== Server is listening on port", port);
+});
